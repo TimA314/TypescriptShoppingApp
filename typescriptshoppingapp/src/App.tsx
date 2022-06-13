0d9a944cbf482183ac1e43452832ddf583f3b9aa
TimA314 <timothyamburn@dev1.mailer.me>
2022-06-12 18:59:59 -0700

    remove default code and add imports

--- typescriptshoppingapp/src/App.tsx
+++ typescriptshoppingapp/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+import { Drawer, Grid } from '@mui/material';
+import LinearProgress from '@mui/material';
+import Badge from '@mui/material';
+import { AddShoppingCart } from '@mui/icons-material';
+import { Wrapper } from './App.styles';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start
     </div>
   );
 }
