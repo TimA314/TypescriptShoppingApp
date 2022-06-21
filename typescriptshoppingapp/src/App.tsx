d9548de86c172aceb6477c9128461ca8999a5344
TimA314 <timothyamburn@dev1.mailer.me>
2022-06-20 17:51:37 -0700

    add placeholder data

--- typescriptshoppingapp/src/App.tsx
+++ typescriptshoppingapp/src/App.tsx
@@ -7,7 +7,21 @@ import Badge from '@mui/material';
 import { AddShoppingCart } from '@mui/icons-material';
 import { Wrapper } from './App.styles';
 
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+const getProducts = async (): Promise<CartItemType[]> => 
+  await(await fetch('https://fakestoreapi.com/products')).json();
+
 function App() {
+  const {data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data);
   return (
     <div className="App">
       Start
